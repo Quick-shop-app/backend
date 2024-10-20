f2045fbb6a614cb47eaeacf297a7dcaacec6b0a6
Dahmani Smail <smail.dahmani.98@gmail.com>
2024-10-21 01:23:53 +0200

    feat: implement product creation functionality with validation

--- src/main/java/ugr/dss/quick_shop/controllers/ProductsController.java
+++ src/main/java/ugr/dss/quick_shop/controllers/ProductsController.java
@@ -1,28 +1,60 @@
 package ugr.dss.quick_shop.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.validation.Valid;
+import ugr.dss.quick_shop.models.Product;
+import ugr.dss.quick_shop.models.ProductDto;
 import ugr.dss.quick_shop.services.ProductsRepository;
 
 @Controller
 @RequestMapping("/products")
 public class ProductsController {
     
-
     @Autowired
     private ProductsRepository productsRepository;
 
     @GetMapping({"", "/", "/index"})
-    public String showProducts(Model model) {
-        model.addAttribute("products", productsRepository.findAll(Sort.by(Sort.Direction.DESC, "id")));
+    public String showProductsPage(Model model) {
+        List<Product> products = productsRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+        model.addAttribute("products", products);
         return "products/index";
     }
 
-    
+    @GetMapping("/create")
+    public String createProductPage(Model model) {
+        ProductDto productDto = new ProductDto();
+        model.addAttribute("productDto", productDto);
 
+        return "products/create";
+    }
+
+    @PostMapping("/create")
+    public String createProduct(@Valid @ModelAttribute ProductDto productDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "products/create";
+        }
+
+        Product product = new Product();
+        product.setName(productDto.getName());
+        product.setBrand(productDto.getBrand());
+        product.setCategory(productDto.getCategory());
+        product.setPrice(productDto.getPrice());
+        product.setDescription(productDto.getDescription());
+        product.setImage(productDto.getImageFile().getOriginalFilename());
+
+        productsRepository.save(product);
+
+        return "redirect:/products/index";
+    }
 }
