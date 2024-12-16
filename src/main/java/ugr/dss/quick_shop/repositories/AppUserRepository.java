77baebe0865e53aedb2d5796a62bf0f3eab9391d
Dahmani Smail <smail.dahmani.98@gmail.com>
2024-12-16 17:12:44 +0100

    feat: add Javadoc comments for findByEmail and findByUsername methods in repositories

--- src/main/java/ugr/dss/quick_shop/repositories/AppUserRepository.java
+++ src/main/java/ugr/dss/quick_shop/repositories/AppUserRepository.java
@@ -5,7 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ugr.dss.quick_shop.models.AppUser;
 
 public interface AppUserRepository extends JpaRepository<AppUser, Integer> {
-
+    /**
+     * Find a user by email
+     * 
+     * @param email
+     * @return
+     */
     public AppUser findByEmail(String email);
 
 }
